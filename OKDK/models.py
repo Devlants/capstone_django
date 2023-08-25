e9716ccf99f0abd9ea792642fb45344771be9f39
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-26 02:06:00 +0900

    s

--- OKDK/models.py
+++ OKDK/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Signal(models.Model):
-    staus = models.BooleanField(default = False)
+    status = models.BooleanField(default = False)
